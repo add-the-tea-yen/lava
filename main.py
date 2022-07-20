a92041196b3dca6864250fc45d581b42b688c0cd
addtheteayen <adityan041414@gmail.com>
2022-07-20 18:20:11 +0530

    main.py ammendments commit

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ nm = str(input("Enter filname: "))
 f = open(nm,'r')
 data = str(f.readlines())
 f.close()
+#doing this because its just easeir
 data = data[2:-2]
 data = data.replace(" ", "")
-print(data)
 m.run(data)
